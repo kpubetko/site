bf5c07242dc91e806def761d2717c2ba0e8e846c
kpubetko <kpubetko00@mail.ru>
2024-04-23 20:41:24 +0700

    кнпоки svg

--- index.php
+++ index.php
@@ -16,28 +16,28 @@
   <header>
     <div id ="logo">
       <a href="http://localhost/site" title="На главную страницу">
-        <img src="img/logo.png" title="TUSUR GYM" alt="TUSUR GYM"></br>
+        <img src="img/logo.svg" title="TUSUR GYM" alt="TUSUR GYM"></br>
         <span>TUSUR GYM</span>
       </a>
     </div>
     <div id="home" class="btn-container">
         <a href="http://localhost/site" title="Домой">
             <div class="btn">
-                <img src="img/home.png" title="Домой" alt="Домой">
+                <img src="img/Home.svg" title="Домой" alt="Домой">
             </div>
         </a>
     </div>
     <div id="gyms" class="btn-container">
         <a href="" title="Залы">
             <div class="btn">
-                <img src="img/gym.png" title="Залы" alt="Залы">
+                <img src="img/Gym.svg" title="Залы" alt="Залы">
             </div>
         </a>
     </div>
     <div id="nutrition" class="btn-container">
         <a href="" title="Питание">
             <div class="btn">
-                <img src="img/food.png" title="Питание" alt="Питание">
+                <img src="img/Food.svg" title="Питание" alt="Питание">
             </div>
         </a>
     </div>
