49810b50bda2e925ca6faf7bc6356d63f3b0011a
kpubetko <kpubetko00@mail.ru>
2024-04-23 15:29:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>TUSUR GYM</title>
+  <link rel="stylesheet" type="text/css" href="css/main.css" charset="utf-8">
+  <meta name="description" content="Упражнения для здорового тела">
+  <meta name="keywords" content="Спорт">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+
+</head>
+
+
+
+<body>
+  <header>
+    <div id ="logo">
+      <a href="http://localhost/site" title="На главную страницу">
+        <span>TUSUR GYM</span>
+      </a>
+    </div>
+    <div id ="home">
+      <a href="http://localhost/site" title="Домой">
+        <div id="btn">
+          <img src="img/home.png" title="Домой" alt="Домой">
+      </a>
+    </div>
+    <div id ="gyms">
+      <a href="" title="Залы">
+        <div id="btn">
+          <img src="img/gym.png" title="Залы" alt="Залы">
+      </a>
+    </div>
+    <div id ="nutrition">
+      <a href="" title="Питание">
+        <div id="btn">
+          <img src="img/food.png" title="Питание" alt="Питание">
+      </a>
+    </div>
+  </header>
+</body>
+</html>
