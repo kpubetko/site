cb82cbcb493562290782a65b8e0db2f506dc6525
Vladimir <vova-gorohov04@mail.ru>
2024-04-24 20:56:54 +0700

    Все Точки Готовы(осталось Вставить ссылки)

--- scripts/script.js
+++ scripts/script.js
@@ -2,19 +2,167 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('blockShoulder').addEventListener('mouseover', function() {
         document.getElementById('tempImage').src = 'img/shoulders_1.svg';
     });
-
     document.getElementById('blockShoulder').addEventListener('mouseout', function() {
         document.getElementById('tempImage').src = 'img/Without.svg';
     });
+
     document.getElementById('blockShoulder2').addEventListener('mouseover', function() {
         document.getElementById('tempImage').src = 'img/shoulders_1.svg';
     });
-
     document.getElementById('blockShoulder2').addEventListener('mouseout', function() {
         document.getElementById('tempImage').src = 'img/Without.svg';
     });
-    
+
+    document.getElementById('blockСhest').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/chest.svg';
+    });
+    document.getElementById('blockСhest').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockBiceps').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/biceps.svg';
+    });
+    document.getElementById('blockBiceps').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockCalves').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/calves.svg';
+    });
+    document.getElementById('blockCalves').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+    document.getElementById('blockCalves2').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/calves.svg';
+    });
+    document.getElementById('blockCalves2').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockForearms').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/forearms.svg';
+    });
+    document.getElementById('blockForearms').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockForearms2').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/forearms.svg';
+    });
+    document.getElementById('blockForearms2').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockGlutes').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/glutes.svg';
+    });
+    document.getElementById('blockGlutes').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockHamstrings').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/hamstrings.svg';
+    });
+    document.getElementById('blockHamstrings').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockLats').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/lats.svg';
+    });
+    document.getElementById('blockLats').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockLowerback').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/lowerback.svg';
+    });
+    document.getElementById('blockLowerback').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockObliques').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/obliques.svg';
+    });
+    document.getElementById('blockObliques').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockQuads').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/quads.svg';
+    });
+    document.getElementById('blockQuads').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockTraps').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/traps.svg';
+    });
+    document.getElementById('blockTraps').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockTraps2').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/traps.svg';
+    });
+    document.getElementById('blockTraps2').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('blockTriceps').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/triceps.svg';
+    });
+    document.getElementById('blockTriceps').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
+
+    document.getElementById('TrapsMiddleTraps').addEventListener('mouseover', function() {
+        document.getElementById('tempImage').src = 'img/traps-middle.svg';
+    });
+    document.getElementById('TrapsMiddleTraps').addEventListener('mouseout', function() {
+        document.getElementById('tempImage').src = 'img/Without.svg';
+    });
 });
 function shoulderLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function chestLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function bicepsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function calvesLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function forearmsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function glutesLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function hamstringsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function latsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function lowerbackLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function obliquesLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function quadsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function trapsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function tricepsLink() {
+    window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
+}
+function trapsMiddleLink() {
     window.location.href = "https://www.iphones.ru/iNotes/editrial-best-shoulder-workout"; // замените ссылку на нужную вам
 }
